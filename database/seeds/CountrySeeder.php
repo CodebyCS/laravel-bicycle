f32f0f67dc31a82b28a70a02c891935543b55954
Carlos Santana <codebycs@gmail.com>
2026-07-14 11:59:45 +0100

    db: create countries table, migration, model and seeder

---
+++ database/seeds/CountrySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Country;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+        public function run()
+        {
+            $countries = [
+                ['name' => 'Portugal'],
+                ['name' => 'Espanha'],
+                ['name' => 'França'],
+                ['name' => 'Polónia']
+            ];
+
+            foreach ($countries as $country){
+            Country::create($country);
+            }
+        }
+
+}
